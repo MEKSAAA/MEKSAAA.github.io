97ebefeba579241978d411bce1eaa7112e3fe3eb
MEKSAAA <140380421+MEKSAAA@users.noreply.github.com>
2026-05-19 09:34:30 +0000

    Deploying to gh-pages from @ MEKSAAA/MEKSAAA.github.io@c23f1bdd04a5a3671da6750a01ce4b34d9777c7d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,16 +391,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-undergraduate-student-majors-in-software-engineering-tongji-univeristy",
+          title: 'Undergraduate student majors in Software Engineering, Tongji Univeristy.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
